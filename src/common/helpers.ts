dcddc65c2019bffde176434366fd5975f8c6c1c2
Seldszar <contact@seldszar.fr>
2022-03-05 18:49:55 +0100

    fix(notifications): fallback & ignore errors

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -6,20 +6,6 @@ export function setupErrorTracking() {
 
   init({
     dsn: process.env.SENTRY_DSN,
-    denyUrls: [/static-cdn\.jtvnw\.net/],
     release: manifest.version,
-  });
-}
-
-export async function readAsDataURL(blob: Blob): Promise<string> {
-  const reader = new FileReader();
-
-  return new Promise((resolve, reject) => {
-    reader.addEventListener("error", reject);
-    reader.addEventListener("load", () => {
-      resolve(reader.result as string);
-    });
-
-    reader.readAsDataURL(blob);
   });
 }
