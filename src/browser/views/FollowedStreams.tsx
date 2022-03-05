61b7823c81aafb4c1e24f1b70df8fae1d7df7d4d
Seldszar <contact@seldszar.fr>
2022-03-05 21:11:09 +0100

    feat: followed streams sorting

--- src/browser/views/FollowedStreams.tsx
+++ src/browser/views/FollowedStreams.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useMemo, useState } from "react";
+import { orderBy } from "lodash-es";
 import tw, { styled } from "twin.macro";
 
 import { filterList, isEmpty } from "@/browser/helpers/array";
@@ -6,6 +7,7 @@ import { useFollowedStreams } from "@/browser/helpers/hooks";
 
 import StreamCard from "@/browser/components/cards/StreamCard";
 import SearchInput from "@/browser/components/SearchInput";
+import Select from "@/browser/components/Select";
 import Splash from "@/browser/components/Splash";
 
 const Wrapper = styled.div`
@@ -16,17 +18,34 @@ const Header = styled.div`
   ${tw`bg-gradient-to-b from-neutral-900 via-neutral-900 to-transparent flex-none p-3 sticky top-0 z-10`}
 `;
 
+const FilterWrapper = styled.div`
+  ${tw`bg-gradient-to-b from-transparent to-black/20 flex gap-6 justify-end py-3 px-4`}
+`;
+
+const FilterSelect = styled(Select)``;
+
 const Item = styled.div``;
 
 const FollowedStreams: FC = () => {
   const [followedStreams, { isLoading }] = useFollowedStreams();
 
   const [searchQuery, setSearchQuery] = useState("");
+  const [sortField, setSortField] = useState("viewer_count");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("desc");
 
-  const filteredStreams = useMemo(
-    () => filterList(followedStreams, ["game_name", "title", "user_login"], searchQuery),
-    [followedStreams, searchQuery]
-  );
+  const filteredStreams = useMemo(() => {
+    let direction = sortDirection;
+
+    if (sortField === "started_at") {
+      direction = direction === "asc" ? "desc" : "asc";
+    }
+
+    return orderBy(
+      filterList(followedStreams, ["game_name", "title", "user_login"], searchQuery),
+      sortField,
+      direction
+    );
+  }, [sortField, sortDirection, followedStreams, searchQuery]);
 
   const children = useMemo(() => {
     if (isLoading) {
@@ -53,6 +72,45 @@ const FollowedStreams: FC = () => {
       <Header>
         <SearchInput onChange={setSearchQuery} />
       </Header>
+
+      <FilterWrapper>
+        <FilterSelect
+          value={sortField}
+          onChange={setSortField}
+          options={[
+            {
+              value: "user_login",
+              label: "Broadcaster",
+            },
+            {
+              value: "game_name",
+              label: "Category",
+            },
+            {
+              value: "started_at",
+              label: "Uptime",
+            },
+            {
+              value: "viewer_count",
+              label: "Viewer Count",
+            },
+          ]}
+        />
+        <FilterSelect
+          value={sortDirection}
+          onChange={setSortDirection}
+          options={[
+            {
+              value: "asc",
+              label: "Ascending",
+            },
+            {
+              value: "desc",
+              label: "Descending",
+            },
+          ]}
+        />
+      </FilterWrapper>
 
       {children}
     </Wrapper>
