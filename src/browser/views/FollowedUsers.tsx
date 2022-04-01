97a1f67da883f705c269caecd4e82abaadbde469
Seldszar <contact@seldszar.fr>
2022-04-01 23:14:13 +0200

    feat: group pinned users

--- src/browser/views/FollowedUsers.tsx
+++ src/browser/views/FollowedUsers.tsx
@@ -1,4 +1,4 @@
-import { find, orderBy } from "lodash-es";
+import { find, groupBy, orderBy } from "lodash-es";
 import React, { FC, useMemo, useState } from "react";
 import tw, { styled } from "twin.macro";
 
@@ -27,6 +27,16 @@ const FilterWrapper = styled.div`
   ${tw`bg-gradient-to-b from-transparent to-black/10 dark:to-black/20 flex gap-6 justify-end py-3 px-4`}
 `;
 
+const Group = styled.div`
+  &::after {
+    ${tw`block border-b border-neutral-200 dark:border-neutral-800 content mx-4 my-1`}
+  }
+
+  &:last-of-type::after {
+    ${tw`hidden`}
+  }
+`;
+
 const FilterSelect = styled(Select)``;
 
 const Item = styled.div``;
@@ -39,26 +49,31 @@ const FollowedUsers: FC = () => {
 
   const [searchQuery, setSearchQuery] = useState("");
 
-  const items = useMemo(() => {
+  const itemGroups = useMemo(() => {
     const users = orderBy(
       filterList(followedUsers, ["description", "login"], searchQuery),
       state.sortField,
       state.sortDirection
     );
 
-    return users.reduce<any[]>((result, user) => {
-      const stream = find(followedStreams, {
-        user_id: user.id,
-        type: "live",
-      });
+    return Object.values(
+      groupBy(
+        users.reduce<any[]>((result, user) => {
+          const stream = find(followedStreams, {
+            user_id: user.id,
+            type: "live",
+          });
 
-      if ([!!stream, null].includes(state.status)) {
-        result.push({ stream, user });
-      }
+          if ([!!stream, null].includes(state.status)) {
+            result.push({ stream, user });
+          }
 
-      return result;
-    }, []);
-  }, [state, followedUsers, followedStreams, searchQuery]);
+          return result;
+        }, []),
+        ({ user }) => (pinnedUsers.includes(user.id) ? 0 : 1)
+      )
+    );
+  }, [state, followedUsers, followedStreams, pinnedUsers, searchQuery]);
 
   const children = useMemo(() => {
     if (isLoading) {
@@ -69,25 +84,29 @@ const FollowedUsers: FC = () => {
       return <Splash>No following users</Splash>;
     }
 
-    if (isEmpty(items)) {
+    if (isEmpty(itemGroups)) {
       return <Splash>No users found</Splash>;
     }
 
     return (
       <>
-        {items.map(({ user, stream }) => (
-          <Item key={user.id}>
-            <UserCard
-              user={user}
-              isLive={!!stream}
-              onTogglePinClick={() => toggle(user.id)}
-              isPinned={pinnedUsers.includes(user.id)}
-            />
-          </Item>
+        {itemGroups.map((items, index) => (
+          <Group key={index}>
+            {items.map(({ user, stream }) => (
+              <Item key={user.id}>
+                <UserCard
+                  user={user}
+                  isLive={!!stream}
+                  onTogglePinClick={() => toggle(user.id)}
+                  isPinned={pinnedUsers.includes(user.id)}
+                />
+              </Item>
+            ))}
+          </Group>
         ))}
       </>
     );
-  }, [items, followedUsers, pinnedUsers, isLoading]);
+  }, [itemGroups, followedUsers, pinnedUsers, isLoading]);
 
   return (
     <Wrapper>
