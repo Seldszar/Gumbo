a1c318c328a4e7464ebf398c9d4b383ec5dda3d6
Seldszar <contact@seldszar.fr>
2022-05-18 01:43:39 +0200

    chore: profile menu icons

--- src/browser/components/ProfileBadge.tsx
+++ src/browser/components/ProfileBadge.tsx
@@ -37,6 +37,12 @@ const ProfileBadge: FC<ProfileBadgeProps> = (props) => {
             {
               type: "link",
               children: "Settings",
+              icon: (
+                <svg viewBox="0 0 24 24">
+                  <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+              ),
               onClick() {
                 setSettingsOpen(true);
               },
@@ -44,6 +50,13 @@ const ProfileBadge: FC<ProfileBadgeProps> = (props) => {
             {
               type: "link",
               children: "About & Help",
+              icon: (
+                <svg viewBox="0 0 24 24">
+                  <circle cx="12" cy="12" r="9" />
+                  <line x1="12" y1="8" x2="12.01" y2="8" />
+                  <polyline points="11 12 12 12 12 16 13 16" />
+                </svg>
+              ),
               onClick() {
                 setAboutOpen(true);
               },
@@ -54,6 +67,12 @@ const ProfileBadge: FC<ProfileBadgeProps> = (props) => {
             {
               type: "link",
               children: "Logout",
+              icon: (
+                <svg viewBox="0 0 24 24">
+                  <path d="M7 6a7.75 7.75 0 1 0 10 0" />
+                  <line x1="12" y1="4" x2="12" y2="12" />
+                </svg>
+              ),
               onClick() {
                 store.set(null);
               },
