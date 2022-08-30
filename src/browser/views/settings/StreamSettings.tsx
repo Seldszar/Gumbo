cc7072619a4ecad5262841a1883797ff05629d6c
Seldszar <contact@seldszar.fr>
2022-08-30 16:04:03 +0200

    fix: settings views

--- src/browser/views/settings/StreamSettings.tsx
+++ src/browser/views/settings/StreamSettings.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from "react";
 import tw, { styled } from "twin.macro";
 
+import { LANGUAGE_OPTIONS } from "@/common/constants";
 import { t } from "@/common/helpers";
 
-import { useFollowedUsers } from "@/browser/helpers/hooks";
-
-import ChannelName from "@/browser/components/ChannelName";
 import CheckboxGrid from "@/browser/components/CheckboxGrid";
 import Section from "@/browser/components/Section";
 import Switch from "@/browser/components/Switch";
@@ -21,35 +19,21 @@ const StyledSwitch = styled(Switch)`
 const StreamSettings: FC = () => {
   const { register, settings } = useSettingsContext();
 
-  const [followedUsers] = useFollowedUsers();
-
   return (
     <Wrapper>
       <Section>
-        <StyledSwitch {...register("notifications.enabled")}>
-          {t("inputLabel_enableNotifications")}
+        <StyledSwitch {...register("streams.withReruns")}>
+          {t("inputLabel_showRerunsInFollowedStreams")}
         </StyledSwitch>
-        <StyledSwitch
-          {...register("notifications.withCategoryChanges")}
-          disabled={!settings.notifications.enabled}
-        >
-          {t("inputLabel_categoryChangeNotifications")}
-        </StyledSwitch>
-        <StyledSwitch
-          {...register("notifications.withFilters")}
-          disabled={!settings.notifications.enabled}
-        >
-          {t("inputLabel_filterNotificationsByChannel")}
+        <StyledSwitch {...register("streams.withFilters")}>
+          {t("inputLabel_filterStreamsByLanguage")}
         </StyledSwitch>
       </Section>
       <Section>
         <CheckboxGrid
-          {...register("notifications.selectedUsers")}
-          disabled={!settings.notifications.enabled || !settings.notifications.withFilters}
-          options={followedUsers.map((user) => ({
-            title: <ChannelName login={user.login} name={user.display_name} />,
-            value: user.id,
-          }))}
+          {...register("streams.selectedLanguages")}
+          disabled={!settings.streams.withFilters}
+          options={LANGUAGE_OPTIONS}
         />
       </Section>
     </Wrapper>
