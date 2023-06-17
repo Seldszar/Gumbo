3e60e0bf517f63ac2222019b46cfee0452003ae0
Seldszar <contact@seldszar.fr>
2023-06-17 20:46:33 +0200

    fix: button theme

--- src/browser/components/Button.tsx
+++ src/browser/components/Button.tsx
@@ -1,6 +1,6 @@
 import { PropsOf } from "@emotion/react";
 import { ReactNode, forwardRef } from "react";
-import tw, { css, styled } from "twin.macro";
+import tw, { styled } from "twin.macro";
 
 import Spinner from "./Spinner";
 
@@ -27,21 +27,17 @@ interface WrapperProps {
 const Wrapper = styled.button<WrapperProps>`
   ${tw`flex gap-2 place-content-center px-4 py-2 relative rounded transition disabled:(cursor-default opacity-25)!`}
 
-  ${(props) =>
-    props.isLoading &&
-    css`
-      ${Inner} {
-        ${tw`invisible`}
-      }
-    `}
+  ${Inner} {
+    ${(props) => props.isLoading && tw`invisible`}
+  }
 
   ${(props) => {
     switch (props.color) {
       case "purple":
-        return tw`bg-purple-500 hover:bg-purple-400 active:bg-purple-600 disabled:bg-purple-500!`;
+        return tw`bg-purple-500 text-white hover:bg-purple-400 active:bg-purple-600 disabled:bg-purple-500!`;
 
       case "red":
-        return tw`bg-red-500 hover:bg-red-400 active:bg-red-600 disabled:bg-red-500!`;
+        return tw`bg-red-500 text-white hover:bg-red-400 active:bg-red-600 disabled:bg-red-500!`;
 
       case "transparent":
         return tw`hover:bg-white/10 active:bg-black/25 disabled:bg-transparent!`;
