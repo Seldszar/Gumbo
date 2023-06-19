8ecee765099ed801b789ad96202301f1954f1cde
Seldszar <contact@seldszar.fr>
2023-06-19 16:36:14 +0200

    refactor: tooltip

--- src/browser/components/Tooltip.tsx
+++ src/browser/components/Tooltip.tsx
@@ -1,7 +1,11 @@
 import {
   FloatingPortal,
-  autoPlacement,
+  Placement,
+  autoUpdate,
+  flip,
   offset,
+  shift,
+  size,
   useFloating,
   useHover,
   useInteractions,
@@ -10,12 +14,15 @@ import {
 import { ReactElement, ReactNode, cloneElement, useState } from "react";
 import tw, { styled } from "twin.macro";
 
+import { remToPixels } from "../helpers";
+
 const Panel = styled.div`
   ${tw`fixed bg-purple-500 max-w-full pointer-events-none px-4 py-2 rounded shadow-lg text-white z-20`}
 `;
 
 interface TooltipProps {
   children: ReactElement;
+  placement?: Placement;
   content?: ReactNode;
 }
 
@@ -23,13 +30,31 @@ function Tooltip(props: TooltipProps) {
   const [isOpen, setOpen] = useState(false);
 
   const { context, floatingStyles, refs } = useFloating({
-    middleware: [autoPlacement(), offset(4)],
-    onOpenChange: setOpen,
-    strategy: "fixed",
     open: isOpen,
+    strategy: "fixed",
+    onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    placement: props.placement,
+    middleware: [
+      flip(),
+      shift({
+        padding: remToPixels(0.5),
+      }),
+      size({
+        padding: remToPixels(0.5),
+        apply({ availableHeight, availableWidth, elements }) {
+          Object.assign(elements.floating.style, {
+            maxHeight: `${availableHeight}px`,
+            maxWidth: `${availableWidth}px`,
+          });
+        },
+      }),
+      offset(remToPixels(0.25)),
+    ],
   });
 
   const hover = useHover(context, {
+    enabled: !!props.content,
     delay: {
       open: 500,
     },
