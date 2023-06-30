4bff1eca64a7c024995dd875820583657034d6f6
Seldszar <contact@seldszar.fr>
2023-06-30 23:13:33 +0200

    refactor: scrollable

--- src/browser/components/Scrollable.tsx
+++ src/browser/components/Scrollable.tsx
@@ -7,19 +7,17 @@ import {
 export type ScrollableProps = OverlayScrollbarsComponentProps;
 
 export function Scrollable(props: ScrollableProps) {
-  return (
-    <OverlayScrollbarsComponent
-      {...defaultsDeep({}, props, {
-        defer: true,
-        options: {
-          scrollbars: {
-            autoHide: "move",
-            theme: "os-theme-gumbo",
-          },
-        },
-      })}
-    />
-  );
+  props = defaultsDeep({}, props, {
+    defer: true,
+    options: {
+      scrollbars: {
+        autoHide: "move",
+        theme: "os-theme-gumbo",
+      },
+    },
+  });
+
+  return <OverlayScrollbarsComponent {...props} />;
 }
 
 export default Scrollable;
