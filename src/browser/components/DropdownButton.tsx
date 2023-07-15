0550f41cb0d1edad8be0d2a203d5d16fcc59620c
Seldszar <contact@seldszar.fr>
2023-07-15 13:48:13 +0200

    fix: dropdown button appearence

--- src/browser/components/DropdownButton.tsx
+++ src/browser/components/DropdownButton.tsx
@@ -3,14 +3,14 @@ import { HTMLAttributes, forwardRef } from "react";
 import tw, { styled } from "twin.macro";
 
 const Button = styled.button`
-  ${tw`bg-white border border-neutral-300 p-1.5 rounded transition shadow-lg hover:(bg-neutral-200 shadow-black/25) dark:(bg-neutral-800 border-neutral-700 hover:bg-neutral-700)`}
+  ${tw`bg-white border border-neutral-300 px-2 rounded transition shadow-lg hover:(bg-neutral-200 shadow-black/25) dark:(bg-neutral-800 border-neutral-700 hover:bg-neutral-700)`}
 `;
 
 export type DropdownButtonProps = HTMLAttributes<HTMLButtonElement>;
 
 const DropdownButton = forwardRef<HTMLButtonElement, DropdownButtonProps>((props, ref) => (
   <Button {...props} ref={ref}>
-    <IconDots size="1.25rem" />
+    <IconDots size="1rem" />
   </Button>
 ));
 
