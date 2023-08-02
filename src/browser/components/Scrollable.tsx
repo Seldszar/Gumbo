fbc63248215e849934272f4c2c72e8760f60d1db
Seldszar <contact@seldszar.fr>
2023-08-02 18:14:58 +0200

    refactor: scrollable

--- src/browser/components/Scrollable.tsx
+++ src/browser/components/Scrollable.tsx
@@ -1,5 +1,5 @@
-import { useOverlayScrollbars } from "overlayscrollbars-react";
-import { useRef, useEffect, ReactNode } from "react";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import { ReactNode } from "react";
 
 export interface ScrollableProps {
   children?: ReactNode;
@@ -7,30 +7,16 @@ export interface ScrollableProps {
 }
 
 export function Scrollable(props: ScrollableProps) {
-  const [initialize] = useOverlayScrollbars({
-    defer: true,
-    options: {
-      scrollbars: {
-        autoHide: "move",
-        theme: "os-theme-gumbo",
-      },
-    },
-  });
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (ref.current == null) {
-      return;
-    }
-
-    initialize(ref.current);
-  }, [initialize]);
-
   return (
-    <div className={props.className} ref={ref}>
-      {props.children}
-    </div>
+    <OverlayScrollbarsComponent
+      options={{
+        scrollbars: {
+          autoHide: "leave",
+          theme: "os-theme-gumbo",
+        },
+      }}
+      {...props}
+    />
   );
 }
 
