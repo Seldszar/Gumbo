5b1ac8da846fbcf982c00ba244c2018d8e6b35d8
Seldszar <contact@seldszar.fr>
2023-08-03 18:33:50 +0200

    fix(scrollable): scrollbars always visible

--- src/browser/components/Scrollable.tsx
+++ src/browser/components/Scrollable.tsx
@@ -11,7 +11,6 @@ export function Scrollable(props: ScrollableProps) {
     <OverlayScrollbarsComponent
       options={{
         scrollbars: {
-          autoHide: "leave",
           theme: "os-theme-gumbo",
         },
       }}
